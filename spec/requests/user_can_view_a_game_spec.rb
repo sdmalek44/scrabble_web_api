fef50e2749a79d59a1c7012051110c748245a251
Stephen Malek <malek.stephen44@gmail.com>
2018-09-12 10:32:25 -0600

    write test for last user story

--- spec/requests/user_can_view_a_game_spec.rb
+++ spec/requests/user_can_view_a_game_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
-
-describe 'GET /api/v1/games/1' do
+describe 'GET /api/v1/games' do
   before(:each) do
     josh = User.create(id: 1, name: "Josh")
     sal = User.create(id: 2, name: "Sal")
@@ -12,17 +11,35 @@ describe 'GET /api/v1/games/1' do
     sal.plays.create(game: game, word: "josh", score: 14)
     sal.plays.create(game: game, word: "no", score: 2)
   end
-  it 'can see game json' do
-    get '/api/v1/games/1'
+  context 'GET /api/v1/games/1' do
+    it 'can see game json' do
+      get '/api/v1/games/1'
 
-    expect(response).to be_success
-    game = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
+      expect(response).to be_success
+      game = JSON.parse(response.body, symbolize_names: true)
 
-    expect(game[:game_id]).to eq(1)
-    expect(game[:scores][0][:user_id]).to eq(1)
-    expect(game[:scores][0][:score]).to eq(15)
-    expect(game[:scores][1][:user_id]).to eq(2)
-    expect(game[:scores][1][:score]).to eq(16)
+      expect(game[:game_id]).to eq(1)
+      expect(game[:scores][0][:user_id]).to eq(1)
+      expect(game[:scores][0][:score]).to eq(15)
+      expect(game[:scores][1][:user_id]).to eq(2)
+      expect(game[:scores][1][:score]).to eq(16)
+    end
+  end
+  context 'POST /api/v1/games/1/plays' do
+    it 'can create a post with a play and update game' do
+      post '/api/v1/games/1/plays?user_id=1&word=at'
+      
+      expect(response).to be(201)
+
+      get '/api/v1/games/1'
+
+      game = JSON.parse(response.body, symbolize_names: true)
+
+      expect(game[:game_id]).to eq(1)
+      expect(game[:scores][0][:user_id]).to eq(1)
+      expect(game[:scores][0][:score]).to eq(17)
+      expect(game[:scores][1][:user_id]).to eq(2)
+      expect(game[:scores][1][:score]).to eq(16)
+    end
   end
 end
